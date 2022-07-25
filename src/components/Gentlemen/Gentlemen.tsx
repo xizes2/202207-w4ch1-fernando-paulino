7585b115372bd2de7403b74e4fee65a878aecb3f
Fernando Paulino <xizes2@hotmail.com>
2022-07-25 23:03:04 +0200

    create gentleman component, interface, change App elements disposition

--- src/components/Gentlemen/Gentlemen.tsx
+++ src/components/Gentlemen/Gentlemen.tsx
@@ -1,88 +1,54 @@
-const Gentlemen = (): JSX.Element => {
+import { useState } from "react";
+import gentlemen from "../../types/interfaces";
+
+interface GentlemenProps {
+  gentlemen: gentlemen;
+}
+
+const Gentlemen = ({ gentlemen }: GentlemenProps): JSX.Element => {
+  const [selected, setSelected] = useState(false);
+
+  const toggleStatus = () => {
+    setSelected(!selected);
+  };
+
   return (
     <main className="main">
       <ul className="gentlemen">
-        <li className="gentleman">
+        <li className="gentleman" onClick={toggleStatus}>
           <div className="gentleman__avatar-container">
             <img
               className="gentleman__avatar"
-              src="img/fary.jpg"
-              alt="The Fary pointing at you"
+              src={`img/${gentlemen.picture}`}
+              alt={gentlemen.alternativeText}
             />
-            <span className="gentleman__initial">F</span>
+            <span className="gentleman__initial">
+              {gentlemen.name.includes("The")
+                ? gentlemen.name.split(" ")[1][0]
+                : gentlemen.name[0]}
+            </span>
           </div>
           <div className="gentleman__data-container">
-            <h2 className="gentleman__name">The Fary</h2>
+            <h2 className="gentleman__name"> {gentlemen.name} </h2>
             <ul className="gentleman__data-list">
               <li className="gentleman__data">
                 <span className="gentleman__data-label">Profession:</span>
-                Influencer
+                {gentlemen.profession}
               </li>
               <li className="gentleman__data">
-                <span className="gentleman__data-label">Status:</span> RIP
-              </li>
-              <li className="gentleman__data">
-                <span className="gentleman__data-label">Twitter:</span> Pending
-              </li>
-            </ul>
-          </div>
-          <i className="icon gentleman__icon fas fa-check"></i>
-          <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
-        </li>
-        <li className="gentleman">
-          <div className="gentleman__avatar-container">
-            <img
-              className="gentleman__avatar"
-              src="img/julio.jpg"
-              alt="Julius Churchs pointing at you"
-            />
-            <span className="gentleman__initial">J</span>
-          </div>
-          <div className="gentleman__data-container">
-            <h2 className="gentleman__name">Julius Churchs</h2>
-            <ul className="gentleman__data-list">
-              <li className="gentleman__data">
-                <span className="gentleman__data-label">Profession:</span>
-                Youtuber
-              </li>
-              <li className="gentleman__data">
-                <span className="gentleman__data-label">Status:</span> Alive
-              </li>
-              <li className="gentleman__data">
-                <span className="gentleman__data-label">Twitter:</span>
-                @julius_churchs
-              </li>
-            </ul>
-          </div>
-          <i className="icon gentleman__icon fas fa-check"></i>
-          <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
-        </li>
-        <li className="gentleman">
-          <div className="gentleman__avatar-container">
-            <img
-              className="gentleman__avatar"
-              src="img/bertin.jpg"
-              alt="Bertin Osbourne pointing at you"
-            />
-            <span className="gentleman__initial">B</span>
-          </div>
-          <div className="gentleman__data-container">
-            <h2 className="gentleman__name">Bertin Osbourne</h2>
-            <ul className="gentleman__data-list">
-              <li className="gentleman__data">
-                <span className="gentleman__data-label">Profession:</span>
-                Java developer
-              </li>
-              <li className="gentleman__data">
-                <span className="gentleman__data-label">Status:</span> Alive
+                <span className="gentleman__data-label">Status:</span>{" "}
+                {gentlemen.status}
               </li>
               <li className="gentleman__data">
                 <span className="gentleman__data-label">Twitter:</span>{" "}
-                @osbourne
+                {gentlemen.twitter}
               </li>
             </ul>
           </div>
-          <i className="icon gentleman__icon fas fa-check"></i>
+          <i
+            className="icon gentleman__icon fas fa-check"
+            style={{ display: selected ? "flex" : "none" }}
+          ></i>
           <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
         </li>
       </ul>
--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -3,13 +3,7 @@ import Button from "../Button/Button";
 const Info = (): JSX.Element => {
   return (
     <>
-      <header className="main-header">
-        <h1 className="main-title">The pointing gentlemen</h1>
-      </header>
-      <section className="controls">
-        <p className="info">0 gentlemen pointing at you</p>
-        <Button />
-      </section>
+      <p className="info">0 gentlemen pointing at you</p>
     </>
   );
 };
